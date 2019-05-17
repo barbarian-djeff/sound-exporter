d43e9aa22d556390d211b50529562e1d894fabb4
Jean-Francois Lentz <jean-francois.lentz@bnpparibasfortis.com>
2019-05-17 08:50:04 +0200

    Prepare template and start collecting data to produce the peaks

--- export.go
+++ export.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"go.uber.org/zap"
+	"time"
 
 	"bufio"
 	"html/template"
 	"io"
-	"math"
 	"net/http"
 	"os"
 	"regexp"
@@ -20,26 +20,85 @@ var (
 
 	maxAverageVolume = 200
 	volumeThreshold  = 2.1
+
+	peaks   = []peak{}
+	minutes = []minute{}
 )
 
+type color string
+
+const (
+	blue  color = "color: #0000ff;"
+	red   color = "color: #ff0000;"
+	green color = "color: #00ff00;"
+)
+
+type peak struct {
+	time    string
+	current volume
+	average volume
+}
+
+func newPeak(t time.Time, curVol int, curCol color, avgVol int, avgCol color) peak {
+	return peak{
+		t.Format("15:04:05"),
+		volume{curVol, curCol},
+		volume{avgVol, avgCol},
+	}
+}
+
+type minute struct {
+	time    string
+	average volume
+}
+
+func newMinute(t time.Time, v int, c color) minute {
+	return minute{
+		t.Format("15:04"),
+		volume{v, c},
+	}
+}
+
+type volume struct {
+	value int
+	color color
+}
+
+type templateData struct {
+	message          string
+	maxAverageVolume int
+	volumeThreshold  float64
+	peaks            []peak
+	minutes          []minute
+}
+
+// data is passed by the reader to the collectors (peak and minute)
 type data struct {
-	Message          string
-	MaxAverageVolume int
-	VolumeThreshold  float64
+	time   time.Time
+	volume int
+	avg    float64
+}
+
+func newData(v int, avg float64) data {
+	return data{time.Now(), v, avg}
 }
 
 func main() {
-	go readVolumes()
+	peakChannel := make(chan data)
+	go collectPeaks(peakChannel)
+	go readVolumes(peakChannel)
 	serveVolumes()
 }
 
 func serveVolumes() {
 	tmpl := template.Must(template.ParseFiles("./html/index.html"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		data := data{
+		data := templateData{
 			"We are good!",
 			maxAverageVolume,
 			volumeThreshold,
+			peaks,
+			minutes,
 		}
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		tmpl.Execute(w, data)
@@ -47,11 +106,18 @@ func serveVolumes() {
 	logger.Fatal("fail to serve", zap.Error(http.ListenAndServe("localhost:8090", nil)))
 }
 
-func readVolumes() {
+func collectPeaks(dataCh chan data) {
+	for {
+		d := <-dataCh
+		logger.Info("record", zap.Time("time", d.time), zap.Int("vol", d.volume), zap.Float64("avg", d.avg))
+	}
+}
+
+func readVolumes(channels ...chan data) {
 	logger.Info("read volumes")
 	reader := bufio.NewReader(os.Stdin)
 
-	var max, sum, count int
+	var sum, count int
 	for {
 		input, err := reader.ReadString('\r')
 		if err != nil {
@@ -61,19 +127,18 @@ func readVolumes() {
 			logger.Fatal("stop", zap.Error(err))
 		}
 		if len(strings.TrimSpace(input)) > 0 {
-			v, ok := readVolume(input)
+			vol, ok := readVolume(input)
 			if !ok {
 				break
 			}
 
 			count++
-			sum += v
+			sum += vol
 			avg := float64(sum) / float64(count)
-			dev := math.Abs(float64(v) - avg)
-			if v > max {
-				max = v
+			data := newData(vol, avg)
+			for _, ch := range channels {
+				ch <- data
 			}
-			logger.Info("record", zap.Int("volume", v), zap.Int("max", max), zap.Float64("average", avg), zap.Float64("dev", dev))
 		}
 	}
 	logger.Info("finish reading")
